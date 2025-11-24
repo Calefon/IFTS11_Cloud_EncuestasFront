2b88a7b449e7cbea7801c6412533445efe5ab2e5
Ezequiel <eze5025@gmail.com>
2025-11-24 17:25:31 -0300

    agregado modo dislexia

--- src/app/components/header-component/header-component.component.ts
+++ src/app/components/header-component/header-component.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, HostListener, inject, signal } from
 import { ThemeService } from '../../services/theme.service';
 import { LanguageService } from '../../services/language.service';
 import { TranslocoModule } from '@jsverse/transloco';
+import { DyslexicFontService } from '../../services/dyslexic-font.service';
 
 @Component({
   selector: 'app-header-component',
@@ -12,9 +13,11 @@ import { TranslocoModule } from '@jsverse/transloco';
 export class HeaderComponentComponent {
   readonly themeService = inject(ThemeService);
   readonly lang = inject(LanguageService);
+  readonly dyslexicService = inject(DyslexicFontService);
 
   langOpen = signal(false);
 
+  toggleDyslexic() {this.dyslexicService.toggleDFont(); }
   toggleTheme() { this.themeService.toggleTheme(); }
   toggleLangMenu() { this.langOpen.update(v => !v); }
   selectLang(code: string) { this.lang.set(code); this.langOpen.set(false); }
